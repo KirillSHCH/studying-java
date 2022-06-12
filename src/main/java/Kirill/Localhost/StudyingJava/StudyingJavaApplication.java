5f12b727b8f58de5ef72b30dd52977fa506595fc
Kirill <vZAF4as7NjpnA8Vuz3T4>
2022-06-12 12:30:17 +0500

    StudyingJava first steps

---
+++ src/main/java/Kirill/Localhost/StudyingJava/StudyingJavaApplication.java
@@ -0,0 +1,13 @@
+package Kirill.Localhost.StudyingJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kirill/Localhost/StudyingJava/StudyingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package Kirill.Localhost.StudyingJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
